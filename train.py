7eda7852515d9d0bc764df96668d6194f475acf0
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-05-06 16:53:28 +0200

    Change default activation function for EDL

--- train.py
+++ train.py
@@ -1288,8 +1288,8 @@ group.add_argument(
 group.add_argument(
     "--edl-activation",
     type=str,
-    default="exp",
-    help='EDL final activation function (default: "exp")',
+    default="softplus",
+    help='EDL final activation function (default: "softplus")',
 )
 
 # Batch norm parameters (only works with gen_efficientnet based models currently)
