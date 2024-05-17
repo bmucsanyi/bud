76b4e88c53e11d955266ee6bf923ef902a6b4bb4
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-05-17 14:15:53 +0200

    Add explicit cross-validation for Laplace

--- bud/wrappers/laplace_wrapper.py
+++ bud/wrappers/laplace_wrapper.py
@@ -1,6 +1,8 @@
 from laplace import Laplace
 from torch import nn
 import torch
+import torch.nn.functional as F
+import numpy as np
 
 from bud.utils.replace import replace
 from bud.wrappers.model_wrapper import PosteriorWrapper
@@ -61,11 +63,20 @@ class LaplaceWrapper(PosteriorWrapper):
             hessian_structure=self.hessian_structure,
         )
         self.laplace_model.fit(train_loader)
-        self.laplace_model.optimize_prior_precision(
-            method=self.prior_optimization_method,
-            val_loader=val_loader,
-            link_approx=self.link_approx,
-        )
+
+        if self.prior_optimization_method == "CV":
+            self.optimize_prior_precision_cv(
+                pred_type=self.pred_type,
+                val_loader=val_loader,
+                link_approx=self.link_approx,
+            )
+        else:
+            self.laplace_model.optimize_prior_precision(
+                method=self.prior_optimization_method,
+                pred_type=self.pred_type,
+                val_loader=val_loader,
+                link_approx=self.link_approx,
+            )
 
     def forward_head(self, *args, **kwargs):
         # Warning! This class requires extra care, as the predictive samples are
@@ -94,3 +105,75 @@ class LaplaceWrapper(PosteriorWrapper):
                 .permute(1, 0, 2),  # [B, S, C]
                 "feature": feature,
             }
+
+    def get_nll(out_dist, targets):
+        return F.nll_loss(
+            out_dist.log().clamp(min=torch.finfo(out_dist.dtype).min), targets
+        )
+
+    def optimize_prior_precision_cv(
+        self,
+        pred_type,
+        val_loader,
+        link_approx="probit",
+        log_prior_prec_min=-4,
+        log_prior_prec_max=4,
+        grid_size=100,
+        n_samples=100,
+    ):
+        interval = torch.logspace(log_prior_prec_min, log_prior_prec_max, grid_size)
+        self.laplace_model.prior_precision = self.gridsearch(
+            interval=interval,
+            val_loader=val_loader,
+            pred_type=pred_type,
+            link_approx=link_approx,
+            n_samples=n_samples,
+        )
+
+        print(f"Optimized prior precision is {self.laplace_model.prior_precision}.")
+
+    def gridsearch(
+        self,
+        interval,
+        val_loader,
+        pred_type,
+        link_approx="probit",
+        n_samples=100,
+    ):
+        results = list()
+        prior_precs = list()
+        for prior_prec in interval:
+            print(f"Trying {prior_prec}...")
+            self.laplace_model.prior_precision = prior_prec
+            try:
+                out_dist, targets = self.validate(
+                    val_loader=val_loader,
+                    pred_type=pred_type,
+                    link_approx=link_approx,
+                    n_samples=n_samples,
+                )
+                result = self.get_nll(out_dist, targets).item()
+            except RuntimeError as error:
+                print(f"Caught an exception in validate: {error}")
+                result = float("inf")
+            results.append(result)
+            prior_precs.append(prior_prec)
+        return prior_precs[np.argmin(results)]
+
+    @torch.no_grad()
+    def validate(
+        self, val_loader, pred_type="glm", link_approx="probit", n_samples=100
+    ):
+        self.laplace_model.model.eval()
+        output_means = list()
+        targets = list()
+        for X, y in val_loader:
+            X, y = X.to(self.laplace_model._device), y.to(self.laplace_model._device)
+            out = self.laplace_model(
+                X, pred_type=pred_type, link_approx=link_approx, n_samples=n_samples
+            )
+
+            output_means.append(out)
+            targets.append(y)
+
+        return torch.cat(output_means, dim=0), torch.cat(targets, dim=0)
